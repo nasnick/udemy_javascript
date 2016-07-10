77070eca01a6a5d4bdc0214c61780762579a4932
nasnick <nas.nick@gmail.com>
2016-07-10 22:53:47 +1200

    first commit

---
+++ B3_GlobalEnvironment/Finished/app.js
@@ -0,0 +1,65 @@
+//var a = 'Hello World!';
+//
+//function b() {
+//    console.log(a);
+//}
+//b();
+
+//var objectLiteral = {
+//    roger: 'nomics',
+//    rad: true
+//}
+//
+//console.log(JSON.stringify(objectLiteral));
+//
+//var jsonvalue = JSON.parse('{ "randy": "lahey", "ricky":"julian"}');
+//console.log(jsonvalue);
+
+var school = {
+    name: "Colenso",
+    principal: "ClearyBalls",
+    badboys: {
+        jade: true,
+        JP: true,
+        nick: true
+}
+};
+console.log(school);
+
+//function greet () {
+//    console.log('hi');
+//};
+//
+//greet.firstname = 'nick';
+//console.log(greet.firstname);
+//greet();
+
+var obj = {
+    greet: 'hello'
+}
+console.log(obj.greet);
+
+//grabbing properties from object
+var prop = 'greet';
+console.log(obj[prop]);
+
+//var arr = [];
+//
+//arr.push(function() {
+//    console.log('hello world 1');
+//});
+//arr.push(function() {
+//    console.log('hello world 2');
+//});
+//arr.push(function() {
+//    console.log('hello world 3');
+//});
+//
+//arr.forEach(function(item){
+//   item(); 
+//});
+
+
+var a = 'roger';
+var b = [];
+var c = {};
